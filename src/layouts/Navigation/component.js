9a2d82049e1ce560e38098981952c505dd5e723c
pmita <panos.mita2@outlook.com>
2022-06-07 12:57:20 +0100

    adding burger menu functionality

--- src/layouts/Navigation/component.js
+++ src/layouts/Navigation/component.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+//ROUTER
+import { Link } from 'react-router-dom';
 //ASSETS
 import Burger from '../../assets/images/svg/burger.svg'
 //STYLES
 import './style.scss';
 
 const Navigation = () => {
+    //STATE & VARIABLES
+    const [isHidden, setIsHidden] = useState(false);
+
+    //EVENTS
+    const handleClick = () => {
+        setIsHidden(!isHidden);
+    }
     return(
         <nav>
-            <img src={Burger} alt='Burger icon for mobiles' className='navigation-burger'/>
-            <a href='/' id="navigation-logo">CandLeaf</a>
-            <ul className='navigation-links active'>
-                <li><a href='/'>Products</a></li>
-                <li><a href='/'>Our Story</a></li>
-                <li><a href='/'>Contact Us</a></li>
+            <img 
+                src={Burger} 
+                alt='Burger icon for mobiles' 
+                className='navigation-burger'
+                onClick={handleClick}
+            />
+
+            <Link 
+                to='/' 
+                id="navigation-logo"
+            >
+                CandLeaf
+            </Link>
+
+            <ul 
+                className={isHidden ? 'navigation-links' : 'navigation-links active'}
+            >
+                <li><Link to='/'>Products</Link></li>
+                <li><Link to='/'>Our Story</Link></li>
+                <li><Link to='/'>Contact Us</Link></li>
             </ul>
-            <ul className='navigation-actions active'>
+
+            <ul 
+                className={isHidden ? 'navigation-actions' : 'navigation-actions active'}
+            >
                 <button className='btn primary'>Login</button>
                 <button className='btn secondary'>Sign Up</button>
             </ul>
