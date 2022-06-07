3003be15f26ad15b3ad46c597bebcf709c3ce85d
pmita <panos.mita2@outlook.com>
2022-06-07 13:08:06 +0100

    adding routes to main pages

---
+++ src/pages/Home/component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const Home = () => {
+    return(
+        <h1>Welcome to Home Page</h1>
+    );
+}
+
+export default Home;
---
+++ src/pages/Products/index.js
@@ -0,0 +1,3 @@
+import Products from "./component";
+
+export default Products;
