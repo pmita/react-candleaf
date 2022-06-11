45666236053621e4bcfd288bf0c79bb9ee70ebbd
pmita <panos.mita2@outlook.com>
2022-06-11 14:23:09 +0100

    adding brands section

--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //COMPONENTS
 import Banner from '../../layouts/Banner/component';
+import Brands from '../../layouts/Brands/component';
 import OurStoryPreview from '../../layouts/OurStoryPreview/component';
 //STYLES
 import './style.scss';
@@ -9,6 +10,7 @@ const Home = () => {
     return(
         <>
             <Banner />
+            <Brands />
             <OurStoryPreview />
         </>
     );
