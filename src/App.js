86f789dce0a0479de79b2ff233c2dbb4f6acde22
pmita <panos.mita2@outlook.com>
2022-06-28 18:09:58 +0100

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-// ROUTER
-import { Routes, Route } from 'react-router-dom';
 // COMPONENTS
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './layouts/Navigation/component';
 // PAGES
 import Home from './pages/Home';
 import Products from './pages/Products';
 import OurStory from './pages/OurStory';
 import ContactUs from './pages/ContactUs';
-import SignIn from "./pages/SignIn/component";
-import SignUp from "./pages/SignUp/component";
-//ROUTER
-import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn/component';
+import SignUp from './pages/SignUp/component';
+// ROUTER
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/our-story' element={<OurStory />} />
-        <Route path='/contact-us' element={<ContactUs />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
