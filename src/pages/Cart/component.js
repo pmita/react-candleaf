6c120ff1258f8993a19c6a1d388ab5fbdc7a4d81
pmita <panos.mita2@outlook.com>
2022-07-12 14:26:50 +0100

    adding transitional rendering to our cart page

--- src/pages/Cart/component.js
+++ src/pages/Cart/component.js
@@ -10,7 +10,7 @@ function Cart() {
   // STATE & VARIABLES
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
-  const { cart } = useSelector((state) => state.cart);
+  const { cart, isPending, error } = useSelector((state) => state.cart);
 
   // useEFFECT
   useEffect(() => {
@@ -18,9 +18,12 @@ function Cart() {
       dispatch(getCartItems(user.uid));
     }
   }, [user, dispatch]);
+
   return (
     <div className="cart-page">
       <h1>Your Shopping Cart</h1>
+      {isPending && <p>Items loading...</p>}
+      {error && <p>{error}</p>}
       {cart && (
         <div className="cart-content">
           {cart.map((item) => <CartCard {...item} key={item.id} />)}
